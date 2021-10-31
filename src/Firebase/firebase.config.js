9e2da17af478965522f95ca44098f1754e9604f6
choyondas <choyondas08@gmail.com>
2021-10-31 15:39:26 +0800

    firebase

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAvcEIrN9XbNKbFyUvWxClk7M95qrebnas",
-    authDomain: "simple-firebase-6fcde.firebaseapp.com",
-    projectId: "simple-firebase-6fcde",
-    storageBucket: "simple-firebase-6fcde.appspot.com",
-    messagingSenderId: "205756616516",
-    appId: "1:205756616516:web:274c93e861e2810da29111"
+    apiKey: "AIzaSyDHhWFFkpHvK3Ic9bN6p6raryUT4ZXQC88",
+    authDomain: "fir-first-project-395f1.firebaseapp.com",
+    projectId: "fir-first-project-395f1",
+    storageBucket: "fir-first-project-395f1.appspot.com",
+    messagingSenderId: "667565076127",
+    appId: "1:667565076127:web:67b8fbf7af79ea05a17caa"
 };
 
 export default firebaseConfig;
